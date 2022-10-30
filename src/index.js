988ab7e56d85f59b2505c0e94a6804a58c2bfb8d
lisa-go <lis-zaa@live.com>
2022-10-30 04:32:22 -0500

    home page done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,92 @@
 import './style.css';
+import icon from './images/icon.svg';
+import menuPage from './menu.js';
 
 
-console.log('hi');
+
+const content = document.querySelector('#content');
+
+
+const tempTest = () => {
+  console.log ('yes');
+}
+
+//* remove current content *//
+const changeContent = () => {
+  content.removeChild(content.children[1]);
+}
+
+//* header creation *//
+const header = document.createElement('div');
+header.classList.add('header');
+content.appendChild(header);
+
+  //* icon *//
+  const myIcon = new Image();
+  myIcon.src = icon;
+  myIcon.classList.add('filter-bw');
+  myIcon.style.objectFit = 'contain';
+  header.appendChild(myIcon);
+
+  //* button bar creation *//
+  const buttonBar = document.createElement('div');
+  buttonBar.classList.add('linkBar');
+  header.appendChild(buttonBar);
+
+    //* creating the buttons *//
+    const btn1 = document.createElement('button');
+    btn1.classList.add('link');
+    btn1.textContent = 'Home';
+    btn1.onclick = changeContent;
+    buttonBar.appendChild(btn1);
+
+    const btn2 = document.createElement('button');
+    btn2.classList.add('link');
+    btn2.textContent = 'Menu';
+    btn2.onclick = menuPage;
+    buttonBar.appendChild(btn2);
+
+    const btn3 = document.createElement('button');
+    btn3.classList.add('link');
+    btn3.textContent = 'Reservations';
+    btn3.onclick = tempTest;
+    buttonBar.appendChild(btn3);
+
+    const btn4 = document.createElement('button');
+    btn4.classList.add('link');
+    btn4.textContent = 'Location';
+    btn4.onclick = tempTest;
+    buttonBar.appendChild(btn4);
+
+
+//* main page* //
+function mainPage() {
+const main = document.createElement('div');
+main.classList.add('main');
+content.appendChild(main);
+main.style.display = 'flex';
+
+  //* logo creation *//
+  const logo = document.createElement('div');
+  logo.classList.add('logo');
+  logo.textContent = 'Hometown Cravings';
+  main.appendChild(logo);
+
+  //* intro piece *//
+  const intro = document.createElement('div');
+  intro.classList.add('intro');
+  intro.innerHTML = 
+  'Get a taste of authentic South East Asian cuisine,' + '<br>' +
+  ' a taste of home away from home.';
+  main.appendChild(intro);
+ 
+
+  //* reservation button *//
+  const mainBtn = document.createElement('button');
+  mainBtn.classList.add('mainBtn');
+  mainBtn.textContent = 'Make Your Reservation Now!';
+  mainBtn.onclick = tempTest;
+  main.appendChild(mainBtn);
+}
+
+mainPage();
---
+++ src/menu.js
@@ -0,0 +1,15 @@
+const menuPage = () => {
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+    content.appendChild(menu);
+    menu.style.display = 'flex';
+    
+      //* logo creation *//
+      const logo = document.createElement('div');
+      logo.classList.add('logo');
+      logo.textContent = 'Hometown Cravings';
+      menu.appendChild(logo);
+    
+    }
+
+export default menuPage;
